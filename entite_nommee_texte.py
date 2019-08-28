ea6f78873b33de85778a8035525efe5845560146
Michele1996 <49474878+Michele1996@users.noreply.github.com>
2019-08-28 09:25:22 +0200

    Update entite_nommee_texte.py

--- entite_nommee_texte.py
+++ entite_nommee_texte.py
@@ -3,5 +3,4 @@ def en_texte(mots):
    os.system("cd /home/mike/mXS")
    os.system("echo \""+mots+"\" | ./bin/tagEtapeModelPLOP.sh > /mnt/c/Users/miche/Desktop/entity.txt")
    file=open('/mnt/c/Users/miche/Desktop/entity.txt')
-   texte = str(b.read())
-   return texte
+   texte_entite = str(file.read())
