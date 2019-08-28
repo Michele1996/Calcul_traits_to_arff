88493447979799c428dfcc9c48b6f316445119c1
Michele1996 <49474878+Michele1996@users.noreply.github.com>
2019-08-28 08:19:01 +0200

    Add files via upload

---
+++ entite_nommee_texte.py
@@ -0,0 +1,7 @@
+def en_texte(mots):
+   import os
+   os.system("cd /home/mike/mXS")
+   os.system("echo \""+mots+"\" | ./bin/tagEtapeModelPLOP.sh > /mnt/c/Users/miche/Desktop/entity.txt")
+   file=open('/mnt/c/Users/miche/Desktop/entity.txt')
+   texte = str(b.read())
+   return texte
