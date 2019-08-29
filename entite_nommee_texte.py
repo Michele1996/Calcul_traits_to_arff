8b16c8cf5770256ecaaa97c5da013bd7ac1279bd
Michele1996 <49474878+Michele1996@users.noreply.github.com>
2019-08-29 10:13:09 +0200

    Delete entite_nommee_texte.py

--- entite_nommee_texte.py
+++
@@ -1,7 +0,0 @@
-#mots est le texte brut du quel on veut savoir les entites nommees
-def en_texte(mots):
-   import os
-   os.system("cd /home/mike/mXS")
-   os.system("echo \""+mots+"\" | ./bin/tagEtapeModelPLOP.sh > /mnt/c/Users/miche/Desktop/entity.txt")
-   file=open('/mnt/c/Users/miche/Desktop/entity.txt')
-   texte_entite = str(file.read())
